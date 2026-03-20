525b20e7992f06a2d71ac805a501e6544bb82d97
Kaffqa <kaffqategar@gmail.com>
2026-03-20 21:40:06 +0700

    fix: Vercel serverless integration, add node types, and fix dot-ts imports

--- api/index.ts
+++ api/index.ts
@@ -2,5 +2,5 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // Re-export Express app for Vercel Serverless Function
-import app from '../server/src/index.js'
+import app from '../server/src/index.ts'
 export default app
