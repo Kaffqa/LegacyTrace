39aaa22b2f41437d18f341269c0669f3021f804c
Kaffqa <kaffqategar@gmail.com>
2026-03-27 17:34:38 +0700

    fix dev

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -5,7 +5,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 const prisma = globalForPrisma.prisma || new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL,
+      url: process.env.DATABASE_URL + (process.env.DATABASE_URL?.includes('?') ? '&' : '?') + 'connect_timeout=10&pool_timeout=10',
     },
   },
   log: process.env.NODE_ENV === 'production' ? ['error'] : ['query', 'error', 'warn'],
